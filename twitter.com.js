df009febfdec9ce606d61b46ce817b58851267d5
Marcelo Andrade <marcelo.andrade.r@gmail.com>
2011-12-09 10:31:52 -0500

    Added swap of usernames/fullnames

--- twitter.com.js
+++ twitter.com.js
@@ -1,3 +1,29 @@
-setInterval(function(){
-	$(".js-stream-item").css("background-color", "");
-}, 5000);
+var MATwitter = {
+	
+	ui_changes : function(){
+		// Remove background of new twitts
+		$(".js-stream-item").css("background-color", "");
+
+		// Swap fullname with @username
+		$(".account-group").each(function () {
+			var fullname = $(this).find(".fullname");
+			if ($(fullname).data("already-swapped") == "yes") return;
+			var username =  $(this).find(".username");
+			var tmp_fullname = $(fullname).html();
+			$(fullname).html($(username).html());
+			$(username).html(tmp_fullname);
+			$(fullname).removeClass("fullname").addClass("username").
+				css("font-size", "15px").
+				css("color", "black");
+			$(username).removeClass("username").addClass("fullname").
+				css("font-size", "12px").
+				css("font-weight", "normal").
+				css("color", "#999");
+				
+			$(username).data("already-swapped", "yes");
+		});
+	}
+	
+};
+MATwitter.ui_changes();
+setInterval(MATwitter.ui_changes, 500);
